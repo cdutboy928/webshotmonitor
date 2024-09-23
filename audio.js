ba38b20cb12a2d526e9e72837e1ff4853850d3bf
cdutboy928 <cdutboy928@gmail.com>
2024-09-23 23:07:36 +0800

    delete all

--- audio.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "alert") {
-    const alertMessage = message.message;
-    const utterance = new SpeechSynthesisUtterance(alertMessage);
-    utterance.lang = "zh-CN";
-    speechSynthesis.speak(utterance);
-  }
-});
